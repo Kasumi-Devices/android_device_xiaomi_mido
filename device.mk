868a8b9757c0531ec2354bf83c38ef4f669149e2
Xisan <ziauddinzisan2001@gmail.com>
2021-12-28 10:10:33 +0000

    Revert "mido: fix build ota package"
    This reverts commit 3cf2717471d0fce1c9bab69d8ce840cb5d60d09b.
    
    Test Build: Error

--- device.mk
+++ device.mk
@@ -17,7 +17,6 @@
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 $(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
-$(call inherit-product, vendor/priv/build.sh)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -428,8 +427,3 @@ PRODUCT_COPY_FILES += \
 # XiaomiParts
 PRODUCT_PACKAGES += \
     XiaomiParts
-
-# OTA
-PRODUCT_PACKAGES += \
-    Updater
-
